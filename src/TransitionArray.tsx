43e578c8387abd66649d1c06892edd045580a19c
conflagrate <jm@memorici.de>
2022-03-03 13:24:25 +0000

    Problem: No imperative example for TransitionArray
    Solution:
     - Imlpement TransitionArrayClass using TransitionArray exports.

--- src/TransitionArray.tsx
+++ src/TransitionArray.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
 import { animated, config, InterpolatorConfig, useTransition } from "react-spring";
 
-const NUM_TRANS: { fig: number, op: any, trans: InterpolatorConfig<any> }[] = [
+export type Items = { fig: number, op: InterpolatorConfig<any>, trans: InterpolatorConfig<any> }[];
+
+export const NUM_TRANS: Items = [
     {
         fig: 1,
         op: { range: [0.75, 1.0], output: [0, 1] },
@@ -100,55 +102,5 @@ function TransitionArray() {
         </div>
     )
 }
-
-// class TransitionArray extends PureComponent {
-//     constructor() {
-//       super()
-
-//       this.state = {
-//         items: NUM_TRANS,
-//       }
-//     }
-
-//     componentDidUpdate(_, prevState) {
-//       const { items: currItems } = this.state
-//       const { items: prevItems } = prevState
-
-//       if (currItems.length === 0 && prevItems.length !== currItems.length) {
-//         setTimeout(() => {
-//           this.setState({
-//             items: NUM_TRANS,
-//           })
-//         }, 2000)
-//       }
-//     }
-
-//     render() {
-//       return (
-//         <div style={{ display: 'flex' }}>
-//           <Transition
-//             items={this.state.items}
-//             from={{ opacity: 0 }}
-//             enter={{ opacity: 1 }}
-//             leave={{ opacity: 0 }}
-//             delay={200}
-//             config={config.molasses}
-//             onRest={() => this.setState({ items: [] })}>
-//             {({ opacity }, item) => (
-//               <animated.div
-//                 style={{
-//                   opacity: opacity.to(item.op),
-//                   transform: opacity
-//                     .to(item.trans)
-//                     .to(y => `translate3d(0,${y}px,0)`),
-//                 }}>
-//                 {item.fig}
-//               </animated.div>
-//             )}
-//           </Transition>
-//         </div>
-//       )
-//     }
-//   }
 
 export default TransitionArray;
