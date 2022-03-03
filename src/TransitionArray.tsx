62e81853947b6d947f67fd0d894fea2c86feb635
conflagrate <jm@memorici.de>
2022-03-03 00:05:03 +0000

    Problem: It's unclear how to use transition configs
    Solution:
     - Add comments about it

--- src/TransitionArray.tsx
+++ src/TransitionArray.tsx
@@ -5,12 +5,48 @@ const NUM_TRANS: { fig: number, op: any, trans: InterpolatorConfig<any> }[] = [
     {
         fig: 1,
         op: { range: [0.75, 1.0], output: [0, 1] },
-        trans: { range: [0.75, 1.0], output: [-40, 0], extrapolate: 'clamp' },
+        trans: { range: [0.0, 1.0], output: [-40, 0], extrapolate: 'clamp' },
     },
     {
-        fig: 2,
-        op: { range: [0.25, 0.5], output: [0, 1] },
-        trans: { range: [0.25, 0.5], output: [-40, 0], extrapolate: 'clamp' },
+        fig: 42,
+        // op here is just "opacity control", it's a transformation that is supplied to `to` function from `FrameValue.d.ts`.
+        // range is something that tells you at which points during the transition animation, which outputs shall be triggered
+        // so you can think about "trans" (used later as "translate3d control") like this:
+        /*
+
+            ┌──────┬──────── -40px
+            │T=0%  │
+            │      │
+            │      │
+            │      │
+            │      │
+            ├──────┼──────── +0px
+            │T=25% │
+            │      │
+            │      │
+            │      │
+            │      │
+            │      │
+            │      │
+            │      │
+            │      │
+            ├──────┼──────── +40px
+            │T=66% │
+            │      │
+            ├──────┼──────── -40px
+            │T=75% │
+            │      │
+            │      │
+            │      │
+            │      │
+            └──────┘
+
+        */
+        // The pixel values are then used to translate over `y` axis via `to` function in animated.div's style.
+        //
+        // Note that you can chain `to`s to fuse together two or more transitions.
+        op: { range: [0.0, 0.9], output: [0, 1] },
+        trans: { range: [0.0, 0.25, 0.66, 0.75], output: [-40, 0, 40, -40], extrapolate: 'clamp' },
     },
     {
         fig: 3,
@@ -20,7 +56,7 @@ const NUM_TRANS: { fig: number, op: any, trans: InterpolatorConfig<any> }[] = [
     {
         fig: 4,
         op: { range: [0.5, 0.75], output: [0, 1] },
-        trans: { range: [0.5, 0.75], output: [-40, 0], extrapolate: 'clamp' },
+        trans: { range: [0.0, 0.75], output: [-40, 0], extrapolate: 'clamp' },
     },
 ];
 
@@ -31,16 +67,20 @@ function TransitionArray() {
         from: { opacity: 0 },
         enter: { opacity: 1 },
         leave: { opacity: 0 },
-        delay: 200,
+        delay: 100,
         config: config.molasses,
         onRest: () => setItems([]),
     })
 
+    const loopDelayMsec = 300;
+
+    // This is a self-dependent loop. Every time setItems gets called, a timeout for another setItems gets called,
+    // which makes an infinite animation loop.
     useEffect(() => {
         if (items.length === 0) {
             setTimeout(() => {
                 setItems(NUM_TRANS)
-            }, 2000)
+            }, loopDelayMsec)
         }
     }, [items])
 
