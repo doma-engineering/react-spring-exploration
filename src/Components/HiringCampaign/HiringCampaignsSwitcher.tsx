7b95a5c3f71fc14e8bb1fdf51038241b4003dfd4
Arsenij Serjogin <mirabilismc@gmail.com>
2022-04-05 00:30:00 +0300

    Problem: Swithing table is not understandable
    Solution:
      - removed duplicated rows,
      - added colors to changed cells.

--- src/Components/HiringCampaign/HiringCampaignsSwitcher.tsx
+++ src/Components/HiringCampaign/HiringCampaignsSwitcher.tsx
@@ -4,7 +4,7 @@ import { currentCompany } from "../../Atoms/Company";
 import { filters, urlFilters } from "../../Atoms/Filters";
 import { selectedType, switcherMouseHoverTable, switcherSelectedTable } from "../../Atoms/HiringCompaign";
 import { tables as allTables } from "../../Atoms/LoadData"
-import { CandidateTableFilters, Company, TablesResult } from "../CandidateTable/candidateTableTypes";
+import { CandidateTableFilters, Company, TableResult, TablesResult } from "../CandidateTable/candidateTableTypes";
 import { defaultFilterParams } from "../CandidateTable/fakeData";
 import { findResult } from "../Validators/HiringCampaignsValidator";
 
@@ -45,6 +45,190 @@ const SwitcherTables = () => {
     return tables.find((table) => table.id === id)?.displayName ?? 'Error finding';
   }
 
+  const getTextColor = (props: {
+    isDifference: boolean,
+    isOld: boolean,
+    isSingle?: boolean,
+  }) => {
+    if (props.isSingle) return styles.colors.textWithoutDiference;
+    if (!props.isDifference) return styles.colors.textDifferenceDefault;
+    if (props.isOld) return styles.colors.textDifferenceOld;
+    return styles.colors.textDifferenceNew;
+  }
+
+  const renderCell = (props: {
+    content: string,
+    key: string,
+    isDifference: boolean,
+    isOld: boolean,
+    isSingle?: boolean,
+  }) => (
+    <td
+      key={props.key}
+      style={{
+        ...styles.tdTh,
+        textAlign: "center",
+        color: getTextColor({
+          isDifference: props.isDifference,
+          isOld: props.isOld,
+          isSingle: props.isSingle
+        })
+      }}
+    >
+      {props.content}
+    </td>
+  )
+
+  const renderRowWithoutDiference = (
+    rowOfTable: TableResult,
+  ) => (
+    <>
+      <tr
+        key={rowOfTable.tableID + "old"}
+        onClick={() => hasClickedOld()}
+        style={{
+          background: styles.colors.rowWithoutDifference,
+        }}
+      >
+
+        {renderCell({
+          content: displayName(rowOfTable.tableID),
+          key: `${rowOfTable.tableID}_name_old`,
+          isOld: true,
+          isDifference: false,
+          isSingle: true,
+        })}
+        {renderCell({
+          content: `${rowOfTable.pending}`,
+          key: `${rowOfTable.tableID}_pend_old`,
+          isOld: true,
+          isDifference: false,
+          isSingle: true,
+        })}
+        {renderCell({
+          content: `${rowOfTable.filtred}`,
+          key: `${rowOfTable.tableID}_filt_old`,
+          isOld: true,
+          isDifference: false,
+          isSingle: true,
+        })}
+        {renderCell({
+          content: `${rowOfTable.total}`,
+          key: `${rowOfTable.tableID}_totl_old`,
+          isOld: true,
+          isDifference: false,
+          isSingle: true,
+        })}
+
+      </tr>
+    </>
+  )
+
+  const renderRowWithDiference = (
+    rowOfTable1: TableResult,
+    rowOfTable2: TableResult,
+    diference: { any: boolean, pending: boolean, filtred: boolean, total: boolean }
+  ) => (
+    <>
+      <tr
+        key={rowOfTable1.tableID + "old"}
+        onClick={() => hasClickedOld()}
+        onMouseEnter={() => setMouseHover(selectedType.old)}
+        onMouseLeave={() => setMouseHover(selectedType.none)}
+        style={{
+          background:
+            mouseHover === selectedType.old ? styles.colors.selectedOld : styles.colors.rowWithDifferenceDefault
+        }}
+      >
+
+        {renderCell({
+          content: displayName(rowOfTable1.tableID),
+          key: `${rowOfTable1.tableID}_name_old`,
+          isOld: true,
+          isDifference: false,
+        })}
+        {renderCell({
+          content: `${rowOfTable1.pending}`,
+          key: `${rowOfTable1.tableID}_pend_old`,
+          isOld: true,
+          isDifference: diference.pending,
+        })}
+        {renderCell({
+          content: `${rowOfTable1.filtred}`,
+          key: `${rowOfTable1.tableID}_filt_old`,
+          isOld: true,
+          isDifference: diference.filtred,
+        })}
+        {renderCell({
+          content: `${rowOfTable1.total}`,
+          key: `${rowOfTable1.tableID}_totl_old`,
+          isOld: true,
+          isDifference: diference.total,
+        })}
+
+      </tr>
+
+      <tr
+        key={rowOfTable2.tableID + "new"}
+        onClick={() => hasClickedNew()}
+        onMouseEnter={() => setMouseHover(selectedType.new)}
+        onMouseLeave={() => setMouseHover(selectedType.none)}
+        style={{
+          background: (mouseHover === selectedType.new) ? styles.colors.selectedNew : styles.colors.rowWithDifferenceDefault
+        }}
+      >
+
+        {renderCell({
+          content: displayName(rowOfTable2.tableID),
+          key: `${rowOfTable2.tableID}_name_new`,
+          isOld: false,
+          isDifference: false,
+        })}
+        {renderCell({
+          content: `${rowOfTable2.pending}`,
+          key: `${rowOfTable2.tableID}_pend_new`,
+          isOld: false,
+          isDifference: diference.pending,
+        })}
+        {renderCell({
+          content: `${rowOfTable2.filtred}`,
+          key: `${rowOfTable2.tableID}_filt_new`,
+          isOld: false,
+          isDifference: diference.filtred,
+        })}
+        {renderCell({
+          content: `${rowOfTable2.total}`,
+          key: `${rowOfTable2.tableID}_totl_new`,
+          isOld: false,
+          isDifference: diference.total,
+        })}
+
+      </tr>
+    </>
+  )
+
+  const renderRow = (rowOfTable1: TableResult, rowOfTable2: TableResult) => {
+
+    const difference = {
+      pending: rowOfTable1.pending !== rowOfTable2.pending,
+      filtred: rowOfTable1.filtred !== rowOfTable2.filtred,
+      total: rowOfTable1.total !== rowOfTable2.total,
+    }
+
+    const haveDifference: boolean =
+      difference.filtred || difference.pending || difference.total;
+
+    if (!haveDifference)
+      return (renderRowWithoutDiference(rowOfTable1))
+    return (
+      renderRowWithDiference(
+        rowOfTable1,
+        rowOfTable2,
+        { any: haveDifference, ...difference }
+      )
+    )
+  }
+
   useEffect(() => {
     setNewTable(
       getHiringTablesResult(company, newFilter)
@@ -55,53 +239,31 @@ const SwitcherTables = () => {
   }
     , []);
 
-  //TODO: each cell five uniq key
-  //TODO style 
   return (
-    <table style={{ ...styles.table, borderCollapse: "collapse", }}>
+    <table
+      className="HiringCompaignsCompare"
+      style={{ ...styles.table, borderCollapse: "collapse", }}
+    >
       <thead>
         <tr>
-          <th style={{ ...styles.tdTh, width: "15rem", }}>Specialization</th>
-          <th style={styles.tdTh}>Pending</th>
-          <th style={styles.tdTh}>Filtred</th>
-          <th style={styles.tdTh}>Total</th>
+          <th key={"HSpecialization"} style={{ ...styles.tdTh, width: "15rem", }}>
+            Specialization
+          </th>
+          <th key={"HPending"} style={styles.tdTh}>
+            Pending
+          </th>
+          <th key={"Hfiltred"} style={styles.tdTh}>
+            Filtred
+          </th>
+          <th key={"Htotal"} style={styles.tdTh}>
+            Total
+          </th>
         </tr>
       </thead>
       <tbody>
         {
           oldTable.map((row, index) =>
-            <>
-              <tr
-                key={row.tableID + "old"}
-                onClick={() => hasClickedOld()}
-                onMouseEnter={() => setMouseHover(selectedType.old)}
-                onMouseLeave={() => setMouseHover(selectedType.none)}
-                style={{
-                  background: mouseHover === selectedType.old ? "#DDAAAA" : "#EEEEEE"
-                }}
-              >
-                <td style={{ ...styles.tdTh, textAlign: "center", }}> {displayName(row.tableID)} </td>
-                <td style={{ ...styles.tdTh, textAlign: "center", }}> {row.pending} </td>
-                <td style={{ ...styles.tdTh, textAlign: "center", }}> {row.filtred} </td>
-                <td style={{ ...styles.tdTh, textAlign: "center", }}> {row.total} </td>
-
-              </tr>
-
-              <tr
-                key={row.tableID + "new"}
-                onClick={() => hasClickedNew()}
-                onMouseEnter={() => setMouseHover(selectedType.new)}
-                onMouseLeave={() => setMouseHover(selectedType.none)}
-                style={{
-                  background: mouseHover === selectedType.new ? "#AADDAA" : "#EEEEEE"
-                }}
-              >
-                <td style={{ ...styles.tdTh, textAlign: "center", }}> {displayName(newTable[index].tableID)} </td>
-                <td style={{ ...styles.tdTh, textAlign: "center", }}> {newTable[index].pending} </td>
-                <td style={{ ...styles.tdTh, textAlign: "center", }}> {newTable[index].filtred} </td>
-                <td style={{ ...styles.tdTh, textAlign: "center", }}> {newTable[index].total} </td>
-              </tr>
-            </>
+            renderRow(row, newTable[index])
           )
         }
       </tbody>
@@ -112,6 +274,16 @@ const SwitcherTables = () => {
 export default SwitcherTables;
 
 const styles = {
+  colors: {
+    selectedOld: "#DDAAAA",
+    selectedNew: "#AADDAA",
+    rowWithDifferenceDefault: "#EEEEEE",
+    rowWithoutDifference: "#CCCCCC",
+    textDifferenceNew: "#058005",
+    textDifferenceOld: "#F02222",
+    textDifferenceDefault: "#000000",
+    textWithoutDiference: "#777777",
+  },
   table: {
     background: "#A0A0C0",
     borders: "1px solid rgb(36, 34, 34)",
