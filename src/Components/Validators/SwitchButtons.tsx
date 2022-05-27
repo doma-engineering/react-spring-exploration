bb2a51d1ee5da5503fcf3af2431748c67860bdf4
Arsenij Serjogin <mirabilismc@gmail.com>
2022-05-27 16:11:22 +0300

    Problem: Switchers butons is'n only for Hirings compaigns
    Solution:
    - Moved to validators folder, that is'n realy good, but a little better

--- src/Components/Validators/SwitchButtons.tsx
+++ src/Components/Validators/SwitchButtons.tsx
@@ -4,7 +4,7 @@ import { selectedType, switcherMouseHoverTable, switcherSelectedTable } from "..
 const SwitherButtons = () => {
 
   const [, setSelected] = useAtom(switcherSelectedTable);
-  const [hover, setHover] = useAtom(switcherMouseHoverTable);
+  const [, setHover] = useAtom(switcherMouseHoverTable);
 
   const hasClickedOld = () => {
     setSelected(selectedType.old);
@@ -15,9 +15,10 @@ const SwitherButtons = () => {
   }
 
   return (
-    <div className="flex justify-center content-center text-slate-300 space-x-2">
+    <div className="flex justify-center content-center text-slate-300 space-x-5">
       <button
-        className="px-2 py-1 outline outline-blue-500 rounded-full outline-red-800 hover:text-slate-200 hover:outline-transparent hover:bg-red-800"
+        className="px-2 py-1 outline rounded-full outline-red-800 
+                 hover:text-slate-200 hover:outline-indigo-500 hover:bg-red-800"
         onMouseEnter={() => setHover(selectedType.old)}
         onMouseLeave={() => setHover(selectedType.none)}
         onClick={hasClickedOld}
@@ -25,7 +26,8 @@ const SwitherButtons = () => {
         select Old
       </button>
       <button
-        className="px-2 py-1 outline outline-blue-500 rounded-full outline-green-700 hover:text-slate-200 hover:outline-transparent hover:bg-green-700"
+        className="px-2 py-1 outline rounded-full outline-green-700 
+                 hover:text-slate-200 hover:outline-indigo-500 hover:bg-green-700"
         onMouseEnter={() => setHover(selectedType.new)}
         onMouseLeave={() => setHover(selectedType.none)}
         onClick={hasClickedNew}
