8005fbd498d53aba307662e7e0cde1299d4e88a8
Arsenij Serjogin <mirabilismc@gmail.com>
2022-06-09 14:30:44 +0300

    Problem: Users wanna sort tables results by their interest
    Solution:
    - TableData before this commit sort candidates by score from hight to low. But now sorting function takes from other atom,
      AND COMMENT: Jotai can't be simple function. They can be as object with field-function.
      but for not create function increase/decrease variation for all sorting methods we also needed know reverted sorting, or not,
      so object there better. {fn: function, isReverted: boolean}. by default function is decrease (first most hight score, close date).
    - Header of table was changed: all centred, part of columns added sorting triangles.
      each triangle can call handleClick, where will changed state (active/passive and up/down) for all triangles:
        - Selected make self active if was passive, and switch direction if is active.
        - Others triangles make self passive.
      handleClick also update atom with sorting function!

--- src/Components/CandidateTable/Table.tsx
+++ src/Components/CandidateTable/Table.tsx
@@ -1,23 +1,103 @@
 import { config, useTransition, animated } from "@react-spring/web";
 import { atom, useAtom } from "jotai";
+import { useState } from "react";
 import { currentTable } from "../../Atoms/CandidateTables";
+import { Candidate } from "../../Atoms/candidateTableTypes";
 import { currentFilters } from "../../Atoms/Filters";
 import { filterData } from "./filterData";
 
 export const tableData = atom(
-  (get) => get(currentTable).table
-    .sort((item1, item2) => (item2.score - item1.score))
-    .filter((candidate) => (
-      filterData
-        .filter((_rank, index) => get(currentFilters)[index])
-        .map(rank => rank.id)
-        .includes(candidate.rank)
-    ))
+  (get) => {
+    const temp = get(currentTable).table
+      .sort((item1, item2) => (get(sortFunction).fn(item1, item2)))
+      .filter((candidate) => (
+        filterData
+          .filter((_rank, index) => get(currentFilters)[index])
+          .map(rank => rank.id)
+          .includes(candidate.rank)
+      ))
+    if (get(sortFunction).isReverse) return temp.reverse();
+    return temp;
+  }
 )
+
+enum SortingMode {
+  incPassive = "△",
+  incActive = "▲",
+  decPassive = "▽",
+  decActive = "▼",
+}
+
+type SortingFunction = (candidate1: Candidate, candidate2: Candidate) => number
+
+type SortingTriangle = {
+  mode: SortingMode,
+  sortingFunction: SortingFunction,
+  isReverse: boolean,
+}
+
+type SortingTriangles = Map<string, SortingTriangle>;
+
+const findNewSortingMode = (before: SortingMode): SortingMode => {
+  switch (before) {
+    case SortingMode.incPassive:
+      return SortingMode.incActive;
+    case SortingMode.incActive:
+      return SortingMode.decActive;
+    case SortingMode.decPassive:
+      return SortingMode.decActive;
+    case SortingMode.decActive:
+      return SortingMode.incActive;
+    default:
+      return SortingMode.decPassive;
+  }
+}
+
+const doPassiveMode = (before: SortingMode) => {
+  if (before === SortingMode.decActive) return SortingMode.decPassive;
+  if (before === SortingMode.incActive) return SortingMode.incPassive;
+  return before;
+}
+
+const getDefaultValuesSortingTriangles = () => {
+  const sortingTriangles: SortingTriangles = new Map<string, SortingTriangle>();
+  sortingTriangles.set("score",
+    {
+      mode: SortingMode.decPassive,
+      sortingFunction: sortByScore,
+      isReverse: false
+    });
+  sortingTriangles.set("scorePercent",
+    {
+      mode: SortingMode.decPassive,
+      sortingFunction: sortByScore,
+      isReverse: false
+    });
+  sortingTriangles.set("date",
+    {
+      mode: SortingMode.decPassive,
+      sortingFunction: sortByDate,
+      isReverse: false
+    });
+  return sortingTriangles;
+}
+
+const sortByScore = (candidate1: Candidate, candidate2: Candidate) =>
+  (candidate2.score - candidate1.score)
+
+const sortByDate = (candidate1: Candidate, candidate2: Candidate) =>
+  (candidate2.taskEndDate.valueOf() - candidate1.taskEndDate.valueOf())
+
+// by default sort function need be decreased (from hight to low values).
+type SortFunction = { fn: SortingFunction, isReverse: boolean }
+const sortFunction = atom<SortFunction>({ fn: sortByScore, isReverse: false });
 
 const Table = () => {
 
   const [candidates] = useAtom(tableData);
+  const [, setSortFunction] = useAtom(sortFunction);
+
+  const [sortingTriangles, setSortingTriangles] = useState<SortingTriangles>(getDefaultValuesSortingTriangles());
 
   const randomOffset = () => {
     return Math.random() > .5 ?
@@ -38,17 +118,76 @@ const Table = () => {
     return rank === undefined ? "FFFFFF" : rank.color;
   }
 
+  const handleClickSortingTriangle = (columName: string) => {
+
+    const currentTriangle = sortingTriangles.get(columName);
+    if (currentTriangle === undefined) return;
+
+    const newTriangleMode = findNewSortingMode(currentTriangle.mode);
+
+    const newSortingTriangles = new Map<string, SortingTriangle>()
+    sortingTriangles.forEach((triangleItem, columKey) => {
+      if (columKey === columName) {
+        newSortingTriangles.set(columKey,
+          {
+            mode: newTriangleMode,
+            sortingFunction: currentTriangle.sortingFunction,
+            isReverse: ((newTriangleMode === SortingMode.incActive) || (newTriangleMode === SortingMode.incPassive))
+          }
+        );
+      } else {
+        newSortingTriangles.set(columKey,
+          {
+            mode: doPassiveMode(triangleItem.mode),
+            sortingFunction: triangleItem.sortingFunction,
+            isReverse: ((triangleItem.mode === SortingMode.incActive) || (triangleItem.mode === SortingMode.incPassive))
+          }
+        );
+      }
+    })
+    setSortingTriangles(newSortingTriangles);
+    setSortFunction({ fn: currentTriangle.sortingFunction, isReverse: newSortingTriangles.get(columName)!.isReverse });
+  }
+
   const formatDate = (date: Date) => (date.toLocaleDateString());
   const formatHash = (hash: string) => (hash.substring(hash.length - 8));
 
   return (
     <div className="tableDiv">
       <div className="tableDivHeaderRow">
-        <div className=" w-20 sm:w-24 md:w-32 lg:w-52 tableDivHeaderCell">Candidate</div>
-        <div className=" w-16 sm:w-20 md:w-24 lg:w-24 tableDivHeaderCell">Score</div>
-        <div className=" w-16 sm:w-20 md:w-24 lg:w-32 tableDivHeaderCell">Score(%)</div>
-        <div className=" w-20 sm:w-32 md:w-36 lg:w-40 tableDivHeaderCell md:px-12">Status</div>
-        <div className=" w-20 sm:w-24 md:w-32 lg:w-52 tableDivHeaderCell">Finish date</div>
+        <div className="flex w-20 sm:w-24 md:w-32 lg:w-52 tableDivHeaderCell items-center justify-center">
+          Candidate
+        </div>
+        <div className="flex w-16 sm:w-20 md:w-24 lg:w-24 tableDivHeaderCell items-center justify-center">
+          <div>Score</div>
+          <div
+            className="text-sm text-center p-2"
+            onClick={() => handleClickSortingTriangle("score")}
+          >
+            {sortingTriangles.get("score")?.mode ?? "#"}
+          </div>
+        </div>
+        <div className="flex w-16 sm:w-20 md:w-24 lg:w-32 tableDivHeaderCell items-center justify-center">
+          Score(%)
+          <div
+            className="text-sm text-center p-2"
+            onClick={() => handleClickSortingTriangle("scorePercent")}
+          >
+            {sortingTriangles.get("scorePercent")?.mode ?? "#"}
+          </div>
+        </div>
+        <div className="flex w-20 sm:w-32 md:w-36 lg:w-40 tableDivHeaderCell md:px-12 items-center justify-center">
+          Status
+        </div>
+        <div className="flex w-20 sm:w-24 md:w-32 lg:w-52 tableDivHeaderCell items-center justify-center">
+          Finish date
+          <div
+            className="text-sm text-center p-2"
+            onClick={() => handleClickSortingTriangle("date")}
+          >
+            {sortingTriangles.get("date")?.mode ?? "#"}
+          </div>
+        </div>
       </div>
       <div>
         {transitions((style, candidate, _) => (
