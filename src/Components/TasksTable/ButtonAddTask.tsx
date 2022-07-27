f80020b056b5fade1c4f7a44feb14c0f57e7a1ad
Arsenij Serjogin <mirabilismc@gmail.com>
2022-07-27 15:43:03 +0300

    Problem: button 'get hiring' is't responsive style
    Solution:
    - Make buttons disabled when task is added.

--- src/Components/TasksTable/ButtonAddTask.tsx
+++ src/Components/TasksTable/ButtonAddTask.tsx
@@ -7,15 +7,25 @@ const ButtonAddTask = ({ taskID }: { taskID: string }) => {
     const hoverClick = () => {
         if (tasks && !tasks.includes(taskID)) setTasks([...tasks, taskID]);
     };
+
+    const ifDisabled = () => {
+        // If user have that task -> return disabled style; else empty string
+        return tasks?.includes(taskID)
+            ? 'lg:bg-slate-700 lg:hover:bg-slate-700 lg:hover:text-slate-300 bg-slate-700 hover:bg-slate-700 hover:text-slate-300'
+            : '';
+    };
+
     return (
         <button
             onClick={hoverClick}
             className="w-full h-full flex justify-center items-center"
         >
-            <div className="btnAccent p-1 md:p-2 lg:hidden">
+            <div className={`btnAccent p-1 md:p-2 lg:hidden ${ifDisabled()}`}>
                 <RiAddFill />
             </div>
-            <div className="hidden lg:block lg:btnAccent lg:py-2 lg:px-6">
+            <div
+                className={`hidden lg:block lg:btnAccent lg:py-2 lg:px-6 ${ifDisabled()}`}
+            >
                 Get
             </div>
         </button>
